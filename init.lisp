743bc1d2c87daa6a7e923225b8eb7d9b8c92ce56
phoebe <phoebe@phoebes-mbp.lan>
2020-12-22 11:56:16 -0600

    asdf source-registry change

--- init.lisp
+++ init.lisp
@@ -9,3 +9,8 @@
 (let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname))))
   (when (probe-file quicklisp-init)
     (load quicklisp-init)))
+
+(asdf:initialize-source-registry
+ '(:source-registry
+   (:tree "~/sift")
+   :inherit-configuration))
