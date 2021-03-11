fb2f30722a6628afb09e2136c149e41e6ba351cb
Phoebe Goldman <phoebe@goldman-tribe.org>
2021-03-11 10:28:50 -0500

    import my utils into the repl

--- init.lisp
+++ init.lisp
@@ -9,3 +9,10 @@
 (let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname))))
   (when (probe-file quicklisp-init)
     (load quicklisp-init)))
+
+(ql:quickload "gefjon-utils")
+
+(handler-bind (#+sbcl
+               (name-conflict (lambda (e) (declare (ignore e))
+                                (invoke-restart 'sb-impl::take-new))))
+  (use-package 'gefjon-utils 'cl-user))
