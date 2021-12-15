fe8f5adff4983b47d2d6c6fc08655289158a0aeb
Phoebe Goldman <phoebe@goldman-tribe.org>
2021-12-15 17:36:06 -0500

    various exts to init.lisp

--- init.lisp
+++ init.lisp
@@ -1,9 +1,14 @@
 ;; -*- mode:lisp; -*-
 
+(cl:in-package :cl-user)
+
 (require 'asdf)
 (require 'uiop)
 
-(setf *print-pretty* t)
+(setf *print-pretty* t
+      *read-default-float-format* 'double-float)
+
+(proclaim '(optimize (debug 2) (speed 2) (safety 2) (space 1) (compilation-speed 1)))
 
 #-quicklisp
 (let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname))))
@@ -11,8 +16,13 @@
     (load quicklisp-init)))
 
 (ql:quickload "gefjon-utils")
+(ql:quickload "alexandria")
 
 (handler-bind (#+sbcl
-               (name-conflict (lambda (e) (declare (ignore e))
-                                (invoke-restart 'sb-impl::take-new))))
+               (sb-ext:name-conflict (lambda (e) (declare (ignore e))
+                                       (invoke-restart 'sb-impl::take-new))))
+  (use-package 'asdf 'cl-user)
+  (use-package 'alexandria 'cl-user)
   (use-package 'gefjon-utils 'cl-user))
+
+(proclaim '(inline alexandria:curry alexandria:rcurry alexandria:compose))
